c2558effb8ee1c55888eab0f8a70297144a003f5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-14 02:28:39 +0300

    Update dtheh class name

--- Producer/Sender.cs
+++ Producer/Sender.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Sender
+{
+    private static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
