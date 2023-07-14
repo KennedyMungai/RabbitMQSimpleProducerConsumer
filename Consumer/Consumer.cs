b80362c84ad2c6dcb0cb41f9a531bf53f69af94c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-14 03:04:50 +0300

    Added the code to create a simple Consumer

--- Consumer/Consumer.cs
+++ Consumer/Consumer.cs
@@ -1,15 +1,31 @@
 ﻿using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
+using System.Text;
 
 namespace Consumer;
 
 public class Consumer
 {
-    private static void Main(string[] args)
+    private static void Main()
     {
-        ConnectionFactory factory = new ConnectionFactory()
+        ConnectionFactory factory = new()
         {
             HostName = "localhost"
+        };
+
+        using var connection = factory.CreateConnection();
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare("BasicTest", false, false, false, null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine("Received message {0}...", message);
+                ;
+            };
         };
     }
 }
